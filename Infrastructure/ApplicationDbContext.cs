6dad1fc14df6f7b885a02516754de379e45c386a
Kayki Letieri <kaykiletieri37@gmail.com>
2024-06-13 09:31:40 -0300

    feat: add initial db context

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using GymCraftAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymCraftAPI.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Exercise> Exercises { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Workout> Workouts { get; set; }
+    public DbSet<WorkoutDay> WorkoutDays { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasAnnotation("Relational:HistoryTableName", "migrations_history");
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
