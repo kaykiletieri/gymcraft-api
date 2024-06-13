7a828c75f25561b78a6aceb70d680f6074f3f6f4
Kayki Letieri <kaykiletieri37@gmail.com>
2024-06-13 09:59:28 -0300

    feat: add initial extensions and ef

---
+++ Infrastructure/Extensions/DataExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GymCraftAPI.Infrastructure.Extensions;
+
+public static class DataExtensions
+{
+    public static IServiceCollection AddEntityFramework(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        return services;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
+using GymCraftAPI.Infrastructure.Extensions;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+string? connectionString = null;
+
+if (builder.Environment.IsDevelopment())
+{
+    connectionString = builder.Configuration["DefaultConnection"];
+}
+
+if (connectionString != null)
+{
+    builder.Services.AddEntityFramework(connectionString);
+}
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
